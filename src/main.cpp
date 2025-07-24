0b95300725514badb436c2e04322e1300d2899f0
Roman <rvs_008@mail.ru>
2025-07-24 19:27:53 +0300

    add memory alocator

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,44 @@
 #include "lib_version.h"
 
 #include <iostream>
+#include <vector>
+#include <map>
+#include "memory_allocator.h"
+
+constexpr int factorial(int n) {
+    return (n == 0) ? 1 : n * factorial(n - 1);
+}
 
 int main(int, char **) {
 	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
+	
+	// map with standart allocator
+	std::map<int, int> std_alloc_map;
+    for (std::size_t i = 0; i < 10; ++i)
+		std_alloc_map[i] = factorial(i);
+
+	for (const auto& [key, value] : std_alloc_map)
+        std::cout << key << " " << value << std::endl;
+    std::cout << std::endl;
+
+    // map with custom allocator
+	using MapAllocator = MemoryAllocator<std::pair<const int, int>, 10>;
+	std::map<int, int, std::less<int>, MapAllocator> cust_alloc_map;
+    for (std::size_t i = 0; i < 10; ++i)
+        cust_alloc_map[i] = factorial(i);
+
+    for (const auto& [key, value] : cust_alloc_map)
+        std::cout << key << " " << value << std::endl;
+    std::cout << std::endl;
+
+	// vector with custom allocator
+	std::vector<int, MemoryAllocator<int,100>> vec;
+	
+	for( int i = 0; i < 10; ++i)
+			vec.push_back(i);
+	
+	for( auto&& v : vec )
+		std::cout << v << " ";
+
 	return 0;
 }
---
+++ src/memory_allocator.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <memory>
+#include <iostream>
+
+
+template <typename T, std::size_t PoolSize>
+class MemoryAllocator
+{
+public:
+    using value_type = T;
+
+    MemoryAllocator()
+	: pool(new char[PoolSize * sizeof(T)])
+	, available_memory(PoolSize * sizeof(T))
+	{
+        #ifndef NDEBUG
+            monitor("Memory pool allocated with size: ", PoolSize);
+        #endif
+    }
+
+    ~MemoryAllocator()
+	{
+        delete[] pool;
+        
+		#ifndef NDEBUG
+            std::cout << "Memory pool deallocated" << std::endl;;
+        #endif
+    }
+
+    template <typename U>
+    MemoryAllocator(const MemoryAllocator<U, PoolSize>&)
+    : pool(nullptr)
+    , available_memory(0)
+    {}
+
+    T* allocate(std::size_t n)
+	{
+        if (n * sizeof(T) > available_memory)
+            throw std::bad_alloc();
+
+        T* result = reinterpret_cast<T*>(pool + (PoolSize * sizeof(T) - available_memory));
+        available_memory -= n * sizeof(T);
+        
+		#ifndef NDEBUG
+            monitor("Allocated ", n);
+        #endif
+		
+		return result;
+    }
+
+    void deallocate([[maybe_unused]]T* ptr, std::size_t n)
+    {
+        #ifndef NDEBUG
+            monitor("Deallocated ", n);
+        #endif
+    }
+
+	template<class U>
+    struct rebind
+    {
+        using other = MemoryAllocator<U, PoolSize>;
+    };
+
+private:
+	void monitor(const std::string& msg, std::size_t n)
+	{
+		std::cout << msg << " " << n << " objects. Available memory remains: " << available_memory << " bytes" << std::endl;
+	}
+
+private:
+    char* pool;
+    std::size_t available_memory;
+};
+
+template <typename T, typename U, std::size_t PoolSize1, std::size_t PoolSize2>
+bool operator==(const MemoryAllocator<T, PoolSize1>& lhs, const MemoryAllocator<U, PoolSize2>& rhs)
+{
+	return (lhs.pool == rhs.pool) && (PoolSize1 == PoolSize2);
+}
+
+template <typename T, typename U, std::size_t PoolSize1, std::size_t PoolSize2>
+bool operator!=(const MemoryAllocator<T, PoolSize1>& lhs, const MemoryAllocator<U, PoolSize2>& rhs)
+{
+    return (lhs.pool != rhs.pool) || (PoolSize1 != PoolSize2);
+}
