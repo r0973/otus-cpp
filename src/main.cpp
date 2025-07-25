aa171b18fad67bf8556a812ffd9383b5efa6495a
r0973 <rvs_008@mail.ru>
2025-07-25 18:10:50 +0300

    custom container example

--- src/main.cpp
+++ src/main.cpp
@@ -15,43 +15,46 @@ int main(int, char **)
 {
 	std::cout << "Version: " << version() << std::endl;
 	
-	// map with standart allocator
-	std::map<int, int> std_alloc_map;
-    for (std::size_t i = 0; i < 10; ++i)
-		std_alloc_map[i] = factorial(i);
+	{
+		// map with standart allocator
+		std::map<int, int> std_alloc_map;
+		for (std::size_t i = 0; i < 10; ++i)
+			std_alloc_map[i] = factorial(i);
 
-	for (const auto& [key, value] : std_alloc_map)
-        std::cout << key << " " << value << std::endl;
-    std::cout << std::endl;
+		for (const auto& [key, value] : std_alloc_map)
+			std::cout << key << " " << value << std::endl;
+		std::cout << std::endl;
 
-    // map with custom allocator
-	using MapAllocator = MemoryAllocator<std::pair<const int, int>, 10>;
-	std::map<int, int, std::less<int>, MapAllocator> cust_alloc_map;
-    for (std::size_t i = 0; i < 10; ++i)
-        cust_alloc_map[i] = factorial(i);
+		// map with custom allocator
+		using MapAllocator = MemoryAllocator<std::pair<const int, int>, 10>;
+		std::map<int, int, std::less<int>, MapAllocator> cust_alloc_map;
+		for (std::size_t i = 0; i < 10; ++i)
+			cust_alloc_map[i] = factorial(i);
 
-    for (const auto& [key, value] : cust_alloc_map)
-        std::cout << key << " " << value << std::endl;
-    std::cout << std::endl;
+		for (const auto& [key, value] : cust_alloc_map)
+			std::cout << key << " " << value << std::endl;
+		std::cout << std::endl;
+	}
 
-	// vector with custom allocator
-	std::vector<int, MemoryAllocator<int,100>> vec;
-	
-	for( int i = 0; i < 10; ++i)
-			vec.push_back(i);
-	
-	for( auto&& v : vec )
-		std::cout << v << " ";
-	std::cout << std::endl; 
+	{
+		// custom container with standart allocator
+		CustomContainer<int> std_alloc_list;
+		for (int i = 0; i < 10; ++i)
+			std_alloc_list.push_back(i);
 
-	// custom container with standart allocator
-	CustomContainer<int> std_alloc_container;
-	for (int i = 0; i < 10; ++i)
-        std_alloc_container.push_back(i);
+		for (auto i : std_alloc_list)
+			std::cout << i << " ";
+		std::cout << std::endl; 
 
-	for (auto i : std_alloc_container)
-        std::cout << i << " ";
-	std::cout << std::endl; 
+		// custom container with custom allocator
+		CustomContainer<int, MemoryAllocator<int, 10>> cust_alloc_list;
+		for (int i = 0; i < 10; ++i)
+			cust_alloc_list.push_back(i);
+
+		for (auto i : cust_alloc_list)
+			std::cout << i << " ";
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
