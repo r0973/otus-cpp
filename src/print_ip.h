d25f7c16300e5522a26fa593ee7e563e4ce5f53c
Roman <rvs_008@mail.ru>
2025-07-29 19:04:30 +0300

    used sfinae for print ip as integer

---
+++ src/print_ip.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <type_traits>
+#include <iostream>
+
+template <typename T>
+std::enable_if_t<std::is_integral<T>::value>
+print_ip(const T& ip_as_int)
+{
+    const size_t size = sizeof(T);
+    
+    for (size_t i = 0; i < size; ++i)
+	{
+        if (i != 0)
+		{
+            std::cout << ".";
+        }
+        int shift = 8 * (size - 1 - i);
+        std::cout << static_cast<int>((ip_as_int >> shift) & 0xFF);
+    }
+    std::cout << std::endl;
+}
