990f69e8a37899ebfc680f8f9cdc5abd69c06f82
r0973 <rvs_008@mail.ru>
2025-11-29 15:49:17 +0300

    take care about input valus of bulkSize

--- src/main.cpp
+++ src/main.cpp
@@ -8,37 +8,43 @@
 
 int main(int argc, char* argv[])
 {
-    if(argc != 2)
+    if (argc != 2)
     {
         std::cerr << "Usage: " << argv[0] << " <bulk_size>" << std::endl;
         return 1;
     }
 
-    std::size_t bulkSize = 0;
+    int bulkSize = 0;
     try
     {
-        bulkSize = static_cast<std::size_t>(std::stoul(argv[1]));
-    } 
-    catch (...)
+        bulkSize = std::stoi(argv[1]);
+        if (bulkSize < 0)
+        {
+            std::cerr << "Error: <bulk_size> must be a positive integer" << std::endl;
+            return 1;
+        }
+                
+        BulkProcessor processor{static_cast<size_t>(bulkSize)};
+
+        auto consoleLogger = std::make_shared<ConsoleLogger>();
+        auto fileLogger = std::make_shared<FileLogger>();
+
+        processor.attach(consoleLogger);
+        processor.attach(fileLogger);
+
+        std::string line;
+        while (std::getline(std::cin, line))
+        {
+            processor.ProcessCommand(Command{line});
+        }
+        processor.Finish();
+
+    }
+    catch(const std::exception& e)
     {
-        std::cerr << "Wrong usage: <bulk_size> must be a positive integer" << std::endl;
+        std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
-    
-    BulkProcessor processor{bulkSize};
-
-    auto consoleLogger = std::make_shared<ConsoleLogger>();
-    auto fileLogger = std::make_shared<FileLogger>();
-
-    processor.attach(consoleLogger);
-    processor.attach(fileLogger);
-
-    std::string line;
-    while (std::getline(std::cin, line))
-    {
-        processor.ProcessCommand(Command{line});
-    }
-    processor.Finish();
 
     return 0;
 }
